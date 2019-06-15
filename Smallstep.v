ce21c5bfcc5869f72e4c937e57b8fe921cdfd810
slasser <samuel.lasser@tufts.edu>
2019-06-14 22:04:13 -0400

    Finish the return case of the smallstep termination proof

--- Smallstep.v
+++ Smallstep.v
@@ -113,38 +113,278 @@ Definition step (tbl : parse_table) (st : state) : step_result :=
     end
   end.
 
-Definition listOfStack (stk : stack) : list frame :=
-  let (fr, frs) := stk in fr :: frs.
+Definition headFrameSize (fr : frame) : nat :=
+  List.length fr.(syms).
 
-Definition gammasOf (frs : list frame) : list (list symbol) :=
-  map syms frs.
+Definition headFrameScore (fr : frame) (b : nat) (e : nat) : nat :=
+  headFrameSize fr * (b ^ e).
 
-Fixpoint gammasScore (gammas : list (list symbol)) (b : nat) (e : nat) :=
-  match gammas with
+Definition tailFrameSize (fr : frame) : nat :=
+  match fr.(syms) with
   | [] => 0
-  | gamma :: gammas' => List.length gamma * ((b + 1) ^ e) + gammasScore gammas' b (e + 1)
+  | _ :: syms' => List.length syms'
   end.
 
-Definition stackScore (stk : stack) b e : nat :=
-  gammasScore (gammasOf (listOfStack stk)) b e.
+Definition tailFrameScore (fr : frame) (b : nat) (e : nat) : nat :=
+  tailFrameSize fr * (b ^ e).
 
-Fixpoint stackPotential (b : nat) (e : nat) : nat :=
-  match e with
-  | O => b * ((b + 1) ^ e)
-  | S e' => b * ((b + 1) ^ e) + stackPotential b e'
+Fixpoint tailFramesScore (frs : list frame) (b : nat) (e : nat) : nat :=
+  match frs with
+  | [] => 0
+  | fr :: frs' => tailFrameScore fr b e + tailFramesScore frs' b (1 + e)
   end.
 
-Definition stateScore (st : state) (b : nat) : nat :=
-  match st with
-  | mkState ts stk avail =>
-    let c := NtSet.cardinal avail in
-    match c with
-    | O => stackScore stk b c
-    | S c' => stackScore stk b c + stackPotential b c'
-    end
-  end.
+Definition stackScore (stk : stack) (b : nat) (e : nat) : nat :=
+  let (hf, tfs) := stk
+  in  headFrameScore hf b e + tailFramesScore tfs b (1 + e).
 
 Ltac inv H := inversion H; subst; clear H.
+
+  Section TripleLT.
+    
+    Variables (A B C : Type) (ltA : relation A) (ltB : relation B) (ltC : relation C).
+    
+    Inductive triple_lex : A * B * C -> A * B * C -> Prop :=
+    | fst_lt : forall x x' y y' z z', ltA x x' -> triple_lex (x, y, z) (x', y', z')
+    | snd_lt : forall x y y' z z', ltB y y' -> triple_lex (x, y, z) (x, y', z')
+    | thd_lt : forall x y z z', ltC z z' -> triple_lex (x, y, z) (x, y, z').
+    
+    Hint Constructors triple_lex.
+    
+    Theorem triple_lex_trans :
+      transitive _ ltA -> transitive _ ltB -> transitive _ ltC -> transitive _ triple_lex.
+    Proof.
+      intros tA tB tC [[x1 y1] z1] [[x2 y2] z2] [[x3 y3] z3] H12 H23.
+      inv H12; inv H23; eauto.
+    Defined.
+    
+    Theorem triple_lex_wf :
+      well_founded ltA -> well_founded ltB -> well_founded ltC -> well_founded triple_lex.
+    Proof.
+      intros wfA wfB wfC [[x y] z].
+      revert y z.
+      induction (wfA x) as [x _ IHx].
+      intros y.
+      induction (wfB y) as [y _ IHy].
+      intros z.
+      induction (wfC z) as [z _ IHz].
+      constructor.
+      intros [[x' y'] z'] H.
+      inv H; eauto.
+    Defined.
+    
+  End TripleLT.
+
+Definition stackHeight (stk : stack) : nat :=
+  let (_, frs) := stk in List.length frs.
+
+Definition meas (st : state) (tbl : parse_table) : nat * nat * nat :=
+  let m := maxEntryLength tbl        in
+  let e := NtSet.cardinal st.(avail) in
+  (List.length st.(tokens), stackScore st.(stk) (1+m) e, stackHeight st.(stk)).
+
+Lemma meas_unfold : 
+  forall st tbl, meas st tbl = (List.length st.(tokens), 
+                                stackScore st.(stk) (1 + maxEntryLength tbl) (NtSet.cardinal st.(avail)),
+                                stackHeight st.(stk)).
+Proof. 
+  auto.
+Qed.
+
+Definition nat_triple_lex : relation (nat * nat * nat) :=
+  triple_lex nat nat nat lt lt lt.
+
+Lemma headFrameScore_nil :
+  forall fr b e,
+    fr.(syms) = [] -> headFrameScore fr b e = 0.
+Proof.
+  intros fr b e Hfr.
+  unfold headFrameScore. unfold headFrameSize.
+rewrite Hfr; auto.
+Qed.
+
+Lemma tailFrameScore_cons :
+  forall fr sym gamma b e,
+    fr.(syms) = sym :: gamma -> tailFrameScore fr b e = List.length gamma * (b ^ e).
+Proof.
+  intros fr sym gamma b e Hfr.
+  unfold tailFrameScore. unfold tailFrameSize.
+  rewrite Hfr; auto.
+Qed.
+
+Lemma stackScore_head_frame_nil :
+  forall fr frs b e, 
+    fr.(syms) = [] 
+    -> stackScore (fr, frs) b e = tailFramesScore frs b (1 + e).
+Proof.
+  intros fr frs b e Hfr.  
+  unfold stackScore. unfold headFrameScore. unfold headFrameSize.
+  rewrite Hfr; simpl; auto.
+Qed.
+
+Lemma stackScore_pre_return :
+  forall fr fr' sym gamma frs b e, 
+    fr.(syms) = nil
+    -> fr'.(syms) = sym :: gamma
+    -> stackScore (fr, fr' :: frs) b e = 
+       (List.length gamma * b ^ (1 + e)) + tailFramesScore frs b (2 + e).
+Proof.
+  intros fr fr' sym gamma frs b e Hfr Hfr'.
+  rewrite stackScore_head_frame_nil; auto.
+  simpl.
+  erewrite tailFrameScore_cons; eauto.
+Qed.
+
+Lemma nonzero_exponents_lt_powers_le :
+  forall b e1 e2, 0 < e1 < e2 -> b ^ e1 <= b ^ e2.
+Proof.
+  intros b e1 e2 [Hlt Hlt']. 
+  destruct b as [| b']. 
+  - destruct e1; destruct e2; auto.
+    omega.
+  - destruct b' as [| b''].
+    + repeat rewrite Nat.pow_1_l; auto.
+    + pose proof Nat.pow_lt_mono_r. 
+      specialize (H (S (S b'')) e1 e2). 
+      assert (fact : forall n m, n < m -> n <= m) by (intros; omega).
+      apply fact.
+      apply Nat.pow_lt_mono_r; auto.
+      omega.
+Qed.
+
+Lemma stackScore_le_after_return :
+  forall callee caller caller' sym gamma frs b e,
+    callee.(syms) = []
+    -> caller.(syms) = sym :: gamma
+    -> caller'.(syms) = gamma
+    -> 0 < e
+    -> stackScore (caller', frs) b e <= stackScore (callee, caller :: frs) b e.
+Proof.
+  intros callee caller caller' sym gamma frs b e Hnil Hcons Htl Hnz.
+  erewrite stackScore_pre_return; eauto.
+  unfold stackScore. unfold headFrameScore. unfold headFrameSize. subst.
+  assert (fact : forall a b c d, a <= c -> b <= d -> a + b <= c + d) by (intros; omega).
+  apply fact.
+  - apply Nat.mul_le_mono_l.
+    apply nonzero_exponents_lt_powers_le; auto.
+  - admit.
+Admitted.
+
+Lemma nonzero_exponents_lt_tailFrameScore_le :
+  forall fr b e1 e2,
+    0 < e1 < e2
+    -> tailFrameScore fr b e1 <= tailFrameScore fr b e2.
+Proof.
+  intros fr b e1 e2 Hlt.
+  unfold tailFrameScore. 
+  apply Nat.mul_le_mono_l.
+  apply nonzero_exponents_lt_powers_le; auto.
+Qed.
+
+Lemma nonzero_exponents_lt_tailFramesScore_le :
+  forall frs b e1 e2,
+    0 < e1 < e2
+    -> tailFramesScore frs b e1 <= tailFramesScore frs b e2.
+Proof.
+  intros frs.
+  induction frs as [| fr frs IH]; intros b e1 e2 Hlt; simpl; auto.
+  apply plus_le_compat.
+  - apply nonzero_exponents_lt_tailFrameScore_le; auto.
+  - apply IH; omega.
+Qed.
+
+Lemma nonzero_exponents_lt_stackScore_le :
+  forall v b e1 e2 e3 e4 frs,
+    0 < e1 < e2
+    -> 0 < e3 < e4
+    -> v * (b ^ e1) + tailFramesScore frs b e3 <= 
+       v * (b ^ e2) + tailFramesScore frs b e4.
+Proof.
+  intros v b e1 e2 e3 e4 frs [H0e1 He1e2] [H0e3 He3e4].
+  apply plus_le_compat.
+  - apply Nat.mul_le_mono_l. 
+    apply nonzero_exponents_lt_powers_le; auto.
+  - apply nonzero_exponents_lt_tailFramesScore_le; auto.
+Qed.
+
+Lemma mem_true_cardinality_neq_0 :
+  forall x s,
+    NtSet.mem x s = true -> NtSet.cardinal s <> 0.
+Proof.
+  intros x s Hm; unfold not; intros Heq.
+  rewrite NtSet.mem_spec in Hm.
+  apply cardinal_inv_1 in Heq.
+  unfold NtSet.Empty in Heq. 
+  eapply Heq; eauto.
+Qed.
+
+Lemma mem_true_cardinality_gt_0 :
+  forall x s,
+    NtSet.mem x s = true -> 0 < NtSet.cardinal s.
+Proof.
+  intros x s Hm.
+  apply mem_true_cardinality_neq_0 in Hm; omega.
+Qed.
+
+  Lemma post_return_state_lt_pre_return_state :
+  forall st st' ts callee caller caller' frs x gamma av tbl,
+    st = mkState ts (callee, caller :: frs) av
+    -> st' = mkState ts (caller', frs) (NtSet.add x av)
+    -> callee.(syms) = []
+    -> caller.(syms) = NT x :: gamma
+    -> caller'.(syms) = gamma
+    -> nat_triple_lex (meas st' tbl) (meas st tbl).
+Proof.
+  intros st st' ts callee caller caller' frs x gamma av tbl Hst Hst' Hnil Hcons Htl; subst.
+  unfold meas; simpl.
+  rewrite headFrameScore_nil with (fr := callee); simpl; auto.
+  erewrite tailFrameScore_cons; eauto.
+  unfold headFrameScore. unfold headFrameSize.
+  destruct (NtSet.mem x av) eqn:Hm.
+  - (* x is already in av, so the cardinality stays the same *)
+    rewrite add_cardinal_1; auto.
+    pose proof nonzero_exponents_lt_stackScore_le as Hle. 
+    specialize (Hle (List.length caller'.(syms))
+                  (S (maxEntryLength tbl)) 
+                  (NtSet.cardinal av)
+                  (S (NtSet.cardinal av))
+                  (S (NtSet.cardinal av))
+                  (S (S (NtSet.cardinal av)))
+                  frs).
+    apply le_lt_or_eq in Hle.
+    + destruct Hle as [Hlt | Heq]; subst.
+      * apply snd_lt; auto.
+      * rewrite Heq.
+        apply thd_lt; auto.
+    + split; auto.
+      eapply mem_true_cardinality_gt_0; eauto.
+    + split; auto.
+      omega.
+  - (* x isn't in av, so the cardinality increase by 1 *)
+    rewrite add_cardinal_2; auto.
+    apply thd_lt; auto.
+Qed.
+
+Lemma step_meas_lt :
+  forall tbl st st',
+    step tbl st = StepK st'
+    -> nat_triple_lex (meas st' tbl) (meas st tbl).
+Proof.
+  intros tbl st st' Hs.
+  unfold step in Hs.
+  destruct st as [ts [fr frs] av].
+  destruct fr as [gamma sv].
+  destruct gamma as [| [y | x] gamma'].
+  - (* return from the current frame *)
+    destruct frs as [| caller frs']; try congruence.
+    destruct caller as [gamma_caller sv_caller]. 
+    destruct gamma_caller as [| [y | x] gamma_caller']; try congruence.
+    inv Hs.
+    eapply post_return_state_lt_pre_return_state; simpl; eauto.
+    simpl; auto.
+  - admit.
+  - admit.
+Admitted.
 
 Lemma add_ain't_empty :
   forall x s,
@@ -153,90 +393,7 @@ Proof.
   unfold not; intros x s He.
   specialize (He x); ND.fsetdec.
 Qed.
-
-
-Lemma foo :
-  forall st st' ts caller caller' callee frs x gamma' av b,
-    st' = mkState ts (caller', frs) (NtSet.add x av)
-    -> st = mkState ts (callee, caller :: frs) av
-    -> callee.(syms) = []
-    -> caller.(syms) = NT x :: gamma'
-    -> caller'.(syms) = gamma'
-    -> stateScore st' b < stateScore st b.
-Proof.
-  intros st st' ts caller caller' callee frs x gamma' av b Hst' Hst Hcallee Hcaller Hcaller'.
-  subst.
-  destruct (NtSet.cardinal (NtSet.add x av)) eqn:Hc.
-  - rewrite <- cardinal_Empty in Hc. exfalso; eapply add_ain't_empty; eauto.
-  - (* we know that (add x av) is non-empty *)
-    simpl.
-    rewrite Hc.
-    destruct (NtSet.mem x av) eqn:Hm.
-    + rewrite add_cardinal_1 in Hc; auto.
-      rewrite Hc.
-      apply plus_lt_compat_r.
-Abort.
-
-Lemma gammasScore_top_frame_nil :
-  forall gamma gammas b e,
-    List.length gamma = 0
-    -> gammasScore (gamma :: gammas) b e = gammasScore gammas b (e + 1).
-Proof.
-  intros gamma gammas b e He.
-  simpl.
-  rewrite He; simpl; auto.
-Qed.
-
-Lemma gammasScore_top_frame_cons :
-  forall gamma sym gamma' gammas b e,
-    gamma = sym :: gamma'
-    -> gammasScore (gamma :: gammas) b e =
-       ((b + 1) ^ e) + List.length gamma' * ((b + 1) ^ e) + gammasScore gammas b (e + 1).
-Proof.
-  intros gamma sym gamma' gammas b e He.
-  subst; simpl; auto.
-Qed.
-  
-Lemma foo :
-  forall caller caller' callee gammas s b e,
-    callee = nil
-    -> caller = s :: caller'
-    -> gammasScore (callee :: caller :: gammas) b e = 
-       gammasScore (caller' :: gammas) b e + (b ^ (e + 1)).
-Proof.
-  intros caller caller' callee gammas s b e Hn Hc.
-  rewrite gammasScore_top_frame_nil.
-  - erewrite gammasScore_top_frame_cons; eauto.
-    simpl. 
-    
-
-Lemma stateScore_lt :
-  forall tbl st st',
-    step tbl st = StepK st'
-    -> stateScore st' (maxEntryLength tbl) <= stateScore st (maxEntryLength tbl).
-Proof.
-  intros tbl st st' Hs.
-  unfold step in Hs.
-  destruct st as [ts [[gamma sv] frs] avail] eqn:Hst.
-  destruct gamma eqn:Hg.
-  - (* returning to the caller frame *)
-    destruct frs as [| [gamma_caller sv_caller] frs']; try congruence.
-    destruct gamma_caller as [| [y | x] gamma_caller']; try congruence.
-    inv Hs.
-    simpl.
-    destruct (NtSet.cardinal (NtSet.add x avail)) eqn:Hc.
-    + exfalso.
-      apply cardinal_Empty in Hc.
-      eapply add_ain't_empty; eauto.
-    + destruct (NtSet.cardinal avail) as [| c'].
-      * simpl in *. omega.
-      apply MP.cardinal_Empty in Hc. 
-      ND.fsetdec.
-    omega.
-    unfold stateScore.
-    simpl.
-    
-         
+             
 let rec run (tbl : parse_table) (stk : stack) (ts : token list) : parse_result =
   match step tbl stk ts with
   | StepAccept (f, ts') -> Accept (f, ts')
