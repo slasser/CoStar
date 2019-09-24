d67b72997c4707b692900fe2026fbf6ea7b16e66
slasser <samuel.lasser@tufts.edu>
2019-09-24 16:04:22 -0400

    progress on parser soundness proof

--- coq/Parser_sound.v
+++ coq/Parser_sound.v
@@ -1,59 +1,24 @@
-Require Import Arith List.
+ Require Import Arith List.
 Require Import GallStar.Defs. 
 Require Import GallStar.Lex.
 Require Import GallStar.Parser.
 Require Import GallStar.Prediction.
 Require Import GallStar.Tactics.
 Require Import GallStar.Termination.
-
 Import ListNotations.
 
-Definition stackSize (stk : p_stack) : nat := 
-  match stk with
-  | (fr, frs) => length (fr :: frs)
+Fixpoint bottomFrame' (h : frame) (t : list frame) : frame :=
+  match t with
+  | []        => h
+  | fr :: frs => bottomFrame' fr frs
   end.
 
-Program Fixpoint bottomFrame (stk : p_stack) {measure (stackSize stk)} : frame :=
-  match stk with
-  | (fr, [])       => fr
-  | (_, fr :: frs) => bottomFrame (fr, frs)
-  end.
+Definition bottomFrame (stk : p_stack) : frame :=
+  let (h, t) := stk in bottomFrame' h t.
 
-Inductive bottom_frame : frame -> p_stack -> Prop :=
-| BF_nil :
-    forall fr,
-      bottom_frame fr (fr, [])
-| BF_cons :
-    forall b_fr fr1 fr2 frs,
-    bottom_frame b_fr (fr2, frs)
-    -> bottom_frame b_fr (fr1, fr2 :: frs).
-
-Lemma test :
-  forall stk a b c,
-    stk = (a, [b; c]) -> bottom_frame c stk.
-Proof.
-  intros stk a b c heq; subst; repeat constructor.
-Qed.
-
-Lemma bottomFrame_rew :
-  forall (stk : p_stack),
-    bottomFrame stk =
-    match stk with
-    | (fr, [])       => fr
-    | (_, fr :: frs) => bottomFrame (fr, frs)
-    end.
-Proof.
-  intros stk.
-  unfold bottomFrame.
-  rewrite Wf.fix_sub_eq.
-  - destruct stk as (fr, frs).
-    simpl.
-    destruct frs as [| fr' frs']; auto.
-  - intros.
-    destruct x as (fr, frs).
-    destruct frs as [| fr' frs']; auto.
-Qed.
-Print Assumptions bottomFrame_rew. (* proof_irrelevance *)
+Definition bottomFrameSyms (stk : p_stack) : list symbol :=
+  let fr := bottomFrame stk
+  in  fr.(loc).(rpre) ++ fr.(loc).(rsuf).
 
 Ltac induct_list_length xs := 
   remember (List.length xs) as l;
@@ -157,9 +122,6 @@ Proof.
   destruct (multistep_cases g st a (Accept f)); auto.
 Qed.
 
-Definition frame_derivation_invar g fr : Prop :=
-  exists wpre, gamma_derivation g fr.(loc).(rpre) wpre fr.(sem).
-
 Lemma step_StepAccept_facts :
   forall g av stk ts v,
     step g (Pst av stk ts) = StepAccept v
@@ -195,6 +157,7 @@ Inductive stack_wf (g : grammar) : p_stack -> Prop :=
       -> stack_wf g (fr, frs) 
       -> stack_wf g (Fr (Loc (Some x) pre suf) v, fr :: frs).
 
+(* MAYBE NOT NECESSARY 
 Lemma stack_derivation_accept_impl_gamma_derivation :
   forall fr stk g wpre av ts v,
   bottom_frame fr stk
@@ -210,6 +173,7 @@ Proof.
   inv hsdp.
   simpl; auto.
 Qed.
+ *)
 
 Inductive stack_derivation_invar (g : grammar) (stk : p_stack) (wsuf w : list token) : Prop :=
 | SD_invar :
@@ -217,6 +181,30 @@ Inductive stack_derivation_invar (g : grammar) (stk : p_stack) (wsuf w : list to
       stack_derivation g stk wpre vpre
       -> wpre ++ wsuf = w
       -> stack_derivation_invar g stk wsuf w.
+
+Lemma return_preserves_stack_derivation_invar :
+  forall g callee caller caller' frs x xo xo_cr pre pre_cr suf_cr v v_cr wsuf w,
+    stack_wf g (callee, caller :: frs)
+    -> callee  = Fr (Loc xo pre []) v
+    -> caller  = Fr (Loc xo_cr pre_cr (NT x :: suf_cr)) v_cr
+    -> caller' = Fr (Loc xo_cr (pre_cr ++ [NT x]) suf_cr) (v_cr ++ [Node x v])
+    -> stack_derivation_invar g (callee, caller :: frs) wsuf w
+    -> stack_derivation_invar g (caller', frs) wsuf w.
+Proof.
+  intros g ce cr cr' frs x xo xo_cr pre pre_cr suf_cr v v_cr wsuf w hwf hce hcr hcr' hi; subst.
+  inv hi.
+  inv H.
+  inv hwf.
+  destruct frs.
+  - econstructor; eauto. 
+    eapply SD_nil. 
+    simpl in *.
+    admit.
+  - simpl in *. 
+    econstructor; eauto. 
+    eapply SD_cons; eauto.
+    
+Admitted.
 
 Lemma step_preserves_stack_derivation_invar :
   forall g av av' stk stk' wsuf wsuf' w,
@@ -235,7 +223,7 @@ Proof.
     + destruct fr_cr as [(xo_cr, pre_cr, suf_cr) v_cr].
       destruct suf_cr as [| [a | x] suf_cr]; tc.
       inv hs.
-      (* return to caller frame preserves invariant *)
+      eapply return_preserves_stack_derivation_invar; eauto.
       admit.
   - destruct s as [a | x].
     + destruct wsuf as [| (a', l) wsuf]; tc.
@@ -259,41 +247,37 @@ Lemma multistep_sound :
          (av   : NtSet.t)
          (stk  : p_stack)
          (a    : Acc lex_nat_triple (Parser.meas g (Pst av stk wsuf)))
-         (v    : forest)
-         (fr   : frame),
-    bottom_frame fr stk
-    -> stack_wf g stk
+         (v    : forest),
+    stack_wf g stk
     -> stack_derivation_invar g stk wsuf w
     -> multistep g (Pst av stk wsuf) a = Accept v
-    -> gamma_derivation g (fr.(loc).(rpre) ++ fr.(loc).(rsuf)) w v.
+    -> gamma_derivation g (bottomFrameSyms stk) w v.
 Proof.
   intros g w wsuf.
   induct_list_length wsuf.
-  intros av stk a v fr hb hw hsd hm.
+  intros av stk a v hw hs hm.
   apply multistep_accept_cases in hm.
-  destruct hm as [hs | he].
+  destruct hm as [hf | he].
   - (* the parser state is in a "final configuration" *)
-    apply step_StepAccept_facts in hs.
-    destruct hs as [[xo [rpre [v' [heq]]]] heq']; subst.
-    inv hb.
-    inv hsd.
+    apply step_StepAccept_facts in hf.
+    destruct hf as [[xo [rpre [v' [heq]]]] heq']; subst.
+    inv hs.
     simpl in *.
+    unfold bottomFrameSyms; simpl.
     inv H.
     repeat rewrite app_nil_r; auto.
   - (* parser is in a non-final configuration *)
-    destruct he as [st' [a' [hs hm]]].
+    destruct he as [st' [a' [hf hm]]].
     destruct st' as [av' stk' wsuf'].
     assert (hl : length wsuf' < length wsuf) by admit.
     assert (hex : exists fr', bottom_frame fr' stk') by admit.
-    apply step_preserves_stack_derivation_invar with (w := w) in hs; auto.
+    apply step_preserves_stack_derivation_invar with (w := w) in hf; auto.
     destruct hex as [fr' hb'].
     eapply IHl with (m := length wsuf')
                     (wsuf := wsuf')
                     (av := av')
-                    (stk := stk')
-                    (fr  := fr') in hl; eauto; clear IHl.
-    + assert (fr.(loc).(rpre) ++ fr.(loc).(rsuf) =
-              fr'.(loc).(rpre) ++ fr'.(loc).(rsuf)) by admit.
+                    (stk := stk') in hl; eauto; clear IHl.
+    + assert (bottomFrameSyms stk = bottomFrameSyms stk') by admit. 
       rewrite H; auto.
     + (* step preserves stack well-formedness *)
       admit.
@@ -309,35 +293,11 @@ Theorem parser_sound :
 Proof.
   intros g ss ts v hp.
   unfold parse in hp.
-  eapply multistep_sound with (w := ts)
-                              (fr := Fr (Loc None [] ss) []) in hp; try (constructor; eauto).
-  - simpl in *; auto.
-  - apply SD_invar with (wpre := []) (vpre := []); auto.
+  eapply multistep_sound with (w := ts) in hp; try (constructor; auto).
+  - unfold bottomFrameSyms; auto.
+  - eapply SD_invar with (wpre := []); auto.
     repeat constructor.
 Qed.
-
-(*
-Lemma return_preserves_stack_derivation_invar :
-  forall g xo rpre rpre2 x rsuf v1 v2 frs w1 w2 w3,
-    stack_derivation_invar g (Fr (Loc xo rpre (NT x :: rsuf
-    stack_derivation g (Fr (Loc xo rpre (NT x :: rsuf)) v, frs) w1 (w2 ++ w3)
-    -> In (x, rpre2) g
-    -> gamma_derivation g rpre2 w2 v2
-    -> stack_derives_prefix g (Fr (Loc xo (rpre ++ [NT x]) rsuf) (v1 ++ [Node x v2]), frs) (w1 ++ w2) w3.
-Proof.
-  intros g xo rpre rpre2 x rsuf v1 v2 frs w1 w2 w3 hi hin hg.
-  inv hi.
-  - constructor.
-    apply bar; auto.
-    assert (happ : w2 = w2 ++ []) by (apply app_nil_r'); rewrite happ.
-    repeat econstructor; eauto.
-  - rewrite <- app_assoc; constructor.
-    + apply bar; auto.
-      assert (happ : w2 = w2 ++ []) by (apply app_nil_r').
-      rewrite happ; repeat econstructor; eauto.
-    + rewrite <- app_assoc; auto.
-Qed.
- *)
 
 Lemma gamma_derivation_app :
   forall g ys1 w1 v1,
@@ -358,240 +318,4 @@ Proof.
 Qed.
 
 Ltac rew_nilr xs := replace xs with (xs ++ []) by apply app_nil_r'.
-
-(*
-Lemma foo :
-  forall g xo rpre rpre2 x rsuf v1 v2 frs w1 w2 w3,
-    stack_derives_prefix g (Fr (Loc xo rpre (NT x :: rsuf)) v1, frs) w1 (w2 ++ w3)
-    -> In (x, rpre2) g
-    -> gamma_derivation g rpre2 w2 v2
-    -> stack_derives_prefix g (Fr (Loc xo (rpre ++ [NT x]) rsuf) (v1 ++ [Node x v2]), frs) (w1 ++ w2) w3.
-Proof.
-  intros g xo rpre rpre2 x rsuf v1 v2 frs w1 w2 w3 hi hin hg.
-  inv hi.
-  - constructor.
-    apply bar; auto.
-    assert (happ : w2 = w2 ++ []) by (apply app_nil_r'); rewrite happ.
-    repeat econstructor; eauto.
-  - rewrite <- app_assoc; constructor.
-    + apply bar; auto.
-      assert (happ : w2 = w2 ++ []) by (apply app_nil_r').
-      rewrite happ; repeat econstructor; eauto.
-    + rewrite <- app_assoc; auto.
-Qed.
- *)
-
-(*
-Lemma foo' :
-  forall g xo pre a suf v frs wpre l wsuf',
-    stack_derives_prefix g
-                         (Fr (Loc xo pre (T a :: suf)) v, frs)
-                         wpre
-                         ((a, l) :: wsuf')
-    -> stack_derives_prefix g
-                            (Fr (Loc xo (pre ++ [T a]) suf) (v ++ [Leaf l]), frs)
-                            (wpre ++ [(a, l)])
-                            wsuf'.
-Proof.
-  intros g xo pre a suf v frs wpre l wsuf' hinv.
-  inv hinv.
-  - constructor.
-    apply bar; auto.
-    (* lemma *)
-    assert (happ : [(a, l)] = [(a, l)] ++ []) by (apply app_nil_r'); rewrite happ; clear happ.
-        assert (happ : [Leaf l] = [Leaf l] ++ []) by (apply app_nil_r'); rewrite happ; clear happ.
-        econstructor; eauto.
-        constructor.
-        constructor.
-  - rewrite <- app_assoc.
-    constructor.
-    + apply bar; auto.
-      assert (happ : [(a, l)] = [(a, l)] ++ []) by (apply app_nil_r'); rewrite happ; clear happ.
-      assert (happ : [Leaf l] = [Leaf l] ++ []) by (apply app_nil_r'); rewrite happ; clear happ.
-      econstructor; eauto.
-      constructor.
-      constructor.
-    + rewrite <- app_assoc; auto.
-Qed.
-*)
-  
-Lemma step_preserves_inv :
-  forall g av av' stk stk' wpre wsuf wsuf',
-    stack_wf g stk
-    -> stack_derives_prefix g stk wpre wsuf
-    -> step g (Pst av stk wsuf) = StepK (Pst av' stk' wsuf')
-    -> exists wpre',
-      wpre ++ wsuf = wpre' ++ wsuf'
-      /\ stack_derives_prefix g stk' wpre' wsuf'.
-Proof.
-  intros g av av' stk stk' wpre wsuf wsuf' hwf hi hs.
-  unfold step in hs.
-  destruct stk as (fr, frs).
-  destruct fr as [(xo, rpre, rsuf) v].
-  destruct rsuf.
-  - destruct frs as [| fr_cr frs].
-    + destruct wsuf; tc.
-    + destruct fr_cr as [(xo_cr, rpre_cr, rsuf_cr) v_cr].
-      destruct rsuf_cr as [| [a | x] rsuf_cr]; tc.
-      (* return case *)
-      inv hs.
-      inv hwf.
-      inv hi.
-      rename wpre0 into wpre.
-      exists (wpre ++ wpre'); split; auto.
-      eapply foo; eauto.
-      rewrite app_nil_r; auto.
-  - destruct s as [a | x].
-    + destruct wsuf as [| (a', l) wsuf]; tc.
-      destruct (t_eq_dec a' a); tc; subst.
-      inv hs.
-      exists (wpre ++ [(a, l)]).
-      split.
-      * rewrite <- app_assoc; auto.
-      * eapply foo'; eauto. 
-    + destruct (NtSet.mem x av); tc.
-      dmeq hpred; tc.
-      * inv hs.
-        exists wpre; split; auto.
-        assert (happ : wpre = wpre ++ []) by apply app_nil_r'; rewrite happ.
-        constructor; auto.
-        constructor.
-      * inv hs.
-        exists wpre; split; auto.
-        assert (happ : wpre = wpre ++ []) by apply app_nil_r'.
-        rewrite happ; constructor; auto.
-        constructor.
-Qed.
-    
-
-(*
-(* maybe we need a four-place derivation relation including a remainder? *)
-Lemma foo :
-  forall g bfr bfr' av av' stk stk' ts ts' vsuf,
-    bottom_frame bfr stk
-    -> bottom_frame bfr' stk'
-    -> step g (Pst av stk ts) = StepK (Pst av' stk' ts')
-    -> gamma_derivation g bfr'.(loc).(rsuf) ts' vsuf
-    -> exists w v,
-        bfr.(sem) ++ v = bfr'.(sem) ++ vsuf
-        /\ gamma_derivation g bfr.(loc).(rsuf) (w ++ ts) v.
-Proof.
-  intros g bfr bfr' av av' stk stk' ts ts' vsuf hbf hbf' hs hg.
-  unfold step in hs.
-  destruct stk as (fr, frs).
-  destruct fr as [[xo pre suf] v].
-  destruct suf as [| [a | x] suf_tl].
-  - destruct frs as [| fr_cr frs].
-    + destruct ts; tc.
-    + destruct fr_cr as [[xo_cr pre_cr suf_cr] v_cr].
-      (* we should know that the frame derivation invariant holds in the callee *)
-      assert (hgd : exists w, gamma_derivation g pre w v) by admit.
-      destruct hgd as [w hgd].
-      destruct suf_cr as [| [a | x] suf_tl_cr]; tc.
-      (* we should also know that (x, pre ++ suf) is a grammar production *)
-      assert (hpr : In (x, pre) g) by admit.
-      inv hs.
-      inv hbf.
-      destruct frs.
-      * inv hbf'; inv H1.
-        simpl in *.
-        exists w; exists (Node x v :: vsuf).
-        split.
-        -- rewrite <- app_assoc; auto.
-        -- constructor; auto.
-           econstructor; eauto.
-      * inv hbf'; inv H1.
-        assert (bfr' = bfr) by admit; subst.
-        exists nil; eexists; split; eauto.
-  - destruct ts as [| (a', l) ts_tl]; tc.
-    destruct (t_eq_dec a' a); tc; subst.
-    inv hs.
-    destruct frs.
-    + inv hbf; inv hbf'.
-      simpl in *.
-      exists []; exists (Leaf l :: vsuf); split.
-      * rewrite <- app_assoc; auto.
-      * simpl.
-        admit.
-    + inv hbf; inv hbf'.
-      assert (bfr = bfr') by admit; subst.
-Abort.
-
-Lemma multistep_sound :
-  forall (g   : grammar)
-         (ts  : list token)
-         (av  : NtSet.t)
-         (stk : p_stack)
-         (a   : Acc lex_nat_triple (meas g (Pst av stk ts)))
-         (v   : forest)
-         (fr  : frame),
-    bottom_frame fr stk
-(*    -> frame_derivation_invar g fr *)
-    -> multistep g (Pst av stk ts) a = Accept v
-    -> exists vsuf,
-      v = fr.(sem) ++ vsuf
-      /\ gamma_derivation g fr.(loc).(rsuf) ts vsuf.
-Proof.
-Abort.
-*)
-
-Lemma multistep_sound :
-  forall (g   : grammar)
-         (ts  : list token)
-         (av  : NtSet.t)
-         (stk : p_stack)
-         (a   : Acc lex_nat_triple (meas g (Pst av stk ts)))
-         (v   : forest)
-         (fr  : frame),
-    bottom_frame fr stk
-(*    -> frame_derivation_invar g fr *)
-    -> multistep g (Pst av stk ts) a = Accept v
-    -> exists vsuf,
-      v = fr.(sem) ++ vsuf
-      /\ gamma_derivation g fr.(loc).(rsuf) ts vsuf.
-Proof.
-  intros g ts.
-  induct_list_length ts.
-  intros av stk a v fr hbf hm.
-  apply multistep_accept_cases in hm.
-  destruct hm as [hs | he].
-  - (* the parser state is in a "final configuration" *)
-    apply step_StepAccept_facts in hs.
-    destruct hs as [[xo [rpre [v' [heq]]]] heq']; subst.
-    inv hbf.
-    exists []; split; simpl; [rewrite app_nil_r; auto | constructor].
-  - (* parser is in a non-final configuration *)
-    destruct he as [st' [a' [hs hm]]].
-    destruct st' as [av' stk' ts'].
-    assert (hl : length ts' < length ts) by admit.
-    assert (hex : exists fr', bottom_frame fr' stk') by admit.
-    destruct hex as [fr' hbf'].
-    eapply IHl with (m := length ts')
-                    (ts := ts')
-                    (av := av')
-                    (stk := stk')
-                    (fr  := fr') in hl; eauto.
-    destruct hl as [vsuf [heq hgd]]; subst.
-    clear IHl.
-    eapply foo; eauto.
-    (* I think the induction hypotheses will give me this one. I might also
-       have to prove that "step" preserves whatever invariant I used in the
-       previous goal. *)
-Admitted.
-
-
-Theorem parser_sound :
-  forall (g : grammar)
-         (ss : list symbol)
-         (ts : list token)
-         (v : forest),
-    parse g ss ts = Accept v
-    -> gamma_derivation g ss ts v.
-Proof.
-  intros g ss ts v hp.
-  unfold parse in hp.
-  eapply multistep_sound with (fr := Fr (Loc None [] ss) []) in hp.
-  - destruct hp as [vsuf [heq hgd]]; subst; simpl in *; auto.
-  - constructor; auto.
-Qed.
   
