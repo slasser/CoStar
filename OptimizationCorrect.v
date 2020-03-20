57a63ea994819c4d1203fe02fc5f7fd7e9f62bbc
Sam Lasser <samuel.lasser@tufts.edu>
2020-03-20 15:23:11 -0400

    commit changes before removing unoptimized version of parser

--- OptimizationCorrect.v
+++ OptimizationCorrect.v
@@ -19,7 +19,41 @@ Module OptimizationCorrectFn (Import D : Defs.T).
     intros cm ps ss ts av un ca a r r' ? hm hm'.
     rewrite multistep_unfold.
     unfold multistep_opt.
-*)
+ *)
+
+  Lemma step_stepopt_equiv_acc :
+    forall g cm ps ss ts av un ca v,
+      step g ps ss ts av un = StepAccept v
+      -> step_opt g cm ps ss ts av un ca = StepAccept_opt v.
+  Proof.
+    intros g cm ps ss ts av un ca v hs.
+    unfold step_opt; unfold step in hs; dmeqs h; inv hs; tc.
+  Qed.
+
+  Lemma step_stepopt_equiv_error :
+    forall g cm ps ss ts av un ca e,
+      step_opt g cm ps ss ts av un ca = StepError_opt e
+      -> step g ps ss ts av un = StepError e.
+  Proof.
+    intros. unfold step_opt in *; unfold step in *; dmeqs h; tc.
+    - admit.
+    - admit.
+    - admit.
+    - admit.
+  Admitted.
+  
+  Lemma step_stepopt_equiv_reject :
+    forall g cm ps ss ts av un ca s,
+      step g ps ss ts av un = StepReject s
+      -> step_opt g cm ps ss ts av un ca = StepReject_opt s
+         \/ exists ps' ss' ts' av' un' ca',
+          step_opt g cm ps ss ts av un ca = StepK_opt ps' ss' ts' av' un' ca'.
+  Proof.
+    intros g cm ps ss ts av un ca v hs.
+    unfold step_opt; unfold step in hs; dmeqs h; inv hs; tc; eauto 20.
+    admit.
+  Admitted.
+  
   (* need invariants about the closure map and cache *)
   Lemma multistep_equiv_multistep_opt' :
     forall g tri (a : Acc lex_nat_triple tri) cm ps ss ts av un ca a',
@@ -37,7 +71,8 @@ Module OptimizationCorrectFn (Import D : Defs.T).
       destruct hm as [[hsa hu] | (? & ? & ? & ? & ? & ? & [hsk hm]) ];
       destruct hm' as [[hsa' hu'] | (? & ? & ? & ? & ? & ? & ? & [hsk' hm'])]. 
       + (* prove correspondence between step and step_opt *)
-        admit.
+        eapply step_stepopt_equiv_acc in hsa; eauto.
+        rewrite hsa in hsa'. inv hsa'. auto.
       + (* contradiction *)
         admit.
       + (* contradiction *)
